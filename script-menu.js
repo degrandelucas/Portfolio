33d7f5e0537f2c3001dee803c7456e770c9abce7
degrandelucas <lucasdegrande15@gmail.com>
2024-05-25 15:08:48 -0300

    menu sempre aparecendo mesmo com media menor que 450px

--- script-menu.js
+++ script-menu.js
@@ -1,28 +1,28 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const iconeMenu = document.querySelector('.icone-menu');
-    const iconeFechar = document.querySelector('.icone-fechar');
-    const nav = document.querySelector('nav');
+// document.addEventListener("DOMContentLoaded", function() {
+//     const iconeMenu = document.querySelector('.icone-menu');
+//     const iconeFechar = document.querySelector('.icone-fechar');
+//     const nav = document.querySelector('nav');
   
-    // Adiciona manipuladores de eventos de clique aos ícones
-    iconeMenu.addEventListener('click', function() {
-      nav.style.display = 'block';
-      iconeMenu.style.display = 'none';
-      iconeFechar.style.display = 'block';
-    });
+//     // Adiciona manipuladores de eventos de clique aos ícones
+//     iconeMenu.addEventListener('click', function() {
+//       nav.style.display = 'block';
+//       iconeMenu.style.display = 'none';
+//       iconeFechar.style.display = 'block';
+//     });
   
-    iconeFechar.addEventListener('click', function() {
-      nav.style.display = 'none';
-      iconeMenu.style.display = 'block';
-      iconeFechar.style.display = 'none';
-    });
+//     iconeFechar.addEventListener('click', function() {
+//       nav.style.display = 'none';
+//       iconeMenu.style.display = 'block';
+//       iconeFechar.style.display = 'none';
+//     });
   
-    // Detecta cliques fora dos elementos relevantes para reverter as alterações
-    document.addEventListener('click', function(event) {
-      if (window.innerWidth < 450 && !event.target.closest('.cabecalho')) {
-        nav.style.display = 'none';
-        iconeMenu.style.display = 'block';
-        iconeFechar.style.display = 'none';
-      }
-    });
-  });
-  //
+//     // Detecta cliques fora dos elementos relevantes para reverter as alterações
+//     document.addEventListener('click', function(event) {
+//       if (window.innerWidth < 450 && !event.target.closest('.cabecalho')) {
+//         nav.style.display = 'none';
+//         iconeMenu.style.display = 'block';
+//         iconeFechar.style.display = 'none';
+//       }
+//     });
+//   });
+  
