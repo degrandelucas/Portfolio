52a29716d312d6ea5287515ebe7f6d101847639e
degrandelucas <lucasdegrande15@gmail.com>
2025-04-21 09:39:53 -0300

    alterando diretorio da pasta public para public_html e tambem colocando no diretorio raiz as img e style.css

--- js/projetos-portfolio.js
+++ js/projetos-portfolio.js
@@ -1,7 +1,7 @@
 export const projetosPortfolio = [
     {
         titulo: 'Plataforma de Cursos Online',
-        imagem: '../public/img/Plataforma-Estudos-ADBelem.png',
+        imagem: '/public_html/img/Plataforma-Estudos-ADBelem.png',
         altImagem: 'Acesso_Login_Plataforma_Estudos_ADBelem',
         descricao: 'Plataforma de E-learning para aprendizado em vídeo, que organiza os cursos divididos em categorias intuitivas. Desenvolvida com backend em PHP [Laravel], banco de dados MySQL e frontend em HTML, CSS e JavaScript.',
         //, oferece busca eficiente, sistema de avaliação por provas e certificação PDF automática
@@ -10,7 +10,7 @@ export const projetosPortfolio = [
     },
     {
         titulo: 'Sorteador por Planilha Online',
-        imagem: '../public/img/Sorteador-Por-Planilha.png',
+        imagem: '/public_html/img/Sorteador-Por-Planilha.png',
         altImagem: 'Interface-do-Sorteador-por-Planilha',
         descricao: 'Esta é uma aplicação web desenvolvida para simplificar e tornar eficiente o processo de sorteio através do carregamento de planilhas. Os usuários podem enviar arquivos em formatos como .xlsx, .xls, .csv e .ods, contendo a lista de participantes. A ferramenta processa esses dados para realizar um sorteio de forma aleatória. O backend da aplicação é construído em PHP, utilizando um banco de dados MySQL para armazenar os resultados, enquanto o frontend dinâmico é desenvolvido com HTML, CSS e JavaScript para uma experiência interativa.',
         linkVerProjeto: 'https://plataformatech.com.br',
@@ -18,7 +18,7 @@ export const projetosPortfolio = [
     },
     {
         titulo: 'Página Grupo de Louvor Renascer',
-        imagem: '../public/img/Pagina-Grupo-De-Louvor.png',
+        imagem: '/public_html/img/Pagina-Grupo-De-Louvor.png',
         altImagem: 'Site_Grupo_De_Louvor',
         descricao: 'Projeto web para o Grupo Renascer (Igreja Assembléia de Deus Belém) que centraliza letras, vídeos e cifras de louvores (ensaiados, próximo ensaio e futuros). Utilizando JavaScript para gerenciar dinamicamente as listas e renderizá-las em uma interface responsiva construída com HTML, CSS e Materialize CSS, o site facilita o acesso e o estudo dos repertórios do grupo.',
         linkVerProjeto: 'https://degrandelucas.github.io/Grupo-Louvor-Igreja/',
@@ -26,7 +26,7 @@ export const projetosPortfolio = [
     },
     {
         titulo: 'Automatização de Tarefas',
-        imagem: '../public/img/Automacao_Log_Python.JPG',
+        imagem: '/public_html/img/Automacao_Log_Python.JPG',
         altImagem: 'Automacao_Site_Logistica_Python',
         descricao: 'Este projeto automatiza tarefas na Plataforma de Tracking da empresa, utilizando Python para criar um script que realiza a finalização de notas fiscais entregues e a adição de observações em notas fiscais não entregues. As principais bibliotecas utilizadas incluem PyAutoGUI e Pandas.',
         linkVerProjeto: 'https://www.linkedin.com/feed/update/urn:li:activity:7212129134548852737/',
