84b2756e89723ba35b38040b3a56315cadb6947d
degrandelucas <lucasdegrande15@gmail.com>
2025-05-28 16:39:38 -0300

    alterando ordem de apresentacao de projetos no portfolio

--- js/projetos-portfolio.js
+++ js/projetos-portfolio.js
@@ -9,20 +9,20 @@ export const projetosPortfolio = [
         linkRepositorio: 'https://github.com/degrandelucas/Plataforma_AD-Belem'
     },
     {
-        titulo: 'Sorteador por Planilha Online',
-        imagem: './img/Sorteador-Por-Planilha.png',
-        altImagem: 'Interface-do-Sorteador-por-Planilha',
-        descricao: 'Esta é uma aplicação web desenvolvida para simplificar e tornar eficiente o processo de sorteio através do carregamento de planilhas. Os usuários podem enviar arquivos em formatos como .xlsx, .xls, .csv e .ods, contendo a lista de participantes. A ferramenta processa esses dados para realizar um sorteio de forma aleatória. O backend da aplicação é construído em PHP, utilizando um banco de dados MySQL para armazenar os resultados, enquanto o frontend dinâmico é desenvolvido com HTML, CSS e JavaScript para uma experiência interativa.',
-        linkVerProjeto: 'https://www.linkedin.com/posts/activity-7309656503852896256-OFdf/?utm_source=share&utm_medium=member_desktop&rcm=ACoAABkRIm4BGsrBZm3OHAei3OUigVS8-XtjyJE',
-        linkRepositorio: 'https://github.com/degrandelucas/SorteadorUploadPlanilha'
-    },
-    {
         titulo: 'Página Grupo de Louvor Renascer',
         imagem: './img/Pagina-Grupo-De-Louvor.png',
         altImagem: 'Site_Grupo_De_Louvor',
         descricao: 'Projeto web para o Grupo Renascer (Igreja Assembléia de Deus Belém) que centraliza letras, vídeos e cifras de louvores (ensaiados, próximo ensaio e futuros). Utilizando JavaScript para gerenciar dinamicamente as listas e renderizá-las em uma interface responsiva construída com HTML, CSS e Materialize CSS, o site facilita o acesso e o estudo dos repertórios do grupo.',
         linkVerProjeto: 'https://degrandelucas.github.io/Grupo-Louvor-Igreja/',
         linkRepositorio: 'https://github.com/degrandelucas/Grupo-Louvor-Igreja'
+    },
+    {
+        titulo: 'Sorteador por Planilha Online',
+        imagem: './img/Sorteador-Por-Planilha.png',
+        altImagem: 'Interface-do-Sorteador-por-Planilha',
+        descricao: 'Esta é uma aplicação web desenvolvida para simplificar e tornar eficiente o processo de sorteio através do carregamento de planilhas. Os usuários podem enviar arquivos em formatos como .xlsx, .xls, .csv e .ods, contendo a lista de participantes. A ferramenta processa esses dados para realizar um sorteio de forma aleatória. O backend da aplicação é construído em PHP, utilizando um banco de dados MySQL para armazenar os resultados, enquanto o frontend dinâmico é desenvolvido com HTML, CSS e JavaScript para uma experiência interativa.',
+        linkVerProjeto: 'https://www.linkedin.com/posts/activity-7309656503852896256-OFdf/?utm_source=share&utm_medium=member_desktop&rcm=ACoAABkRIm4BGsrBZm3OHAei3OUigVS8-XtjyJE',
+        linkRepositorio: 'https://github.com/degrandelucas/SorteadorUploadPlanilha'
     },
     {
         titulo: 'Automatização de Tarefas',
