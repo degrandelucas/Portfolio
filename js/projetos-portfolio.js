55658bdd1f67c8e00c3e95f5c2579ca64bb78900
degrandelucas <lucasdegrande15@gmail.com>
2025-06-07 10:07:19 -0300

    alterando ordem dos projetos na pagina

--- js/projetos-portfolio.js
+++ js/projetos-portfolio.js
@@ -1,52 +1,57 @@
 export const projetosPortfolio = [
-    {
-        titulo: 'Plataforma de Cursos Online',
-        imagem: './img/Plataforma-Estudos-ADBelem.png',
-        altImagem: 'Acesso_Login_Plataforma_Estudos_ADBelem',
-        descricao: 'Plataforma de E-learning para aprendizado em vídeo, que organiza os cursos divididos em categorias intuitivas. Desenvolvida com backend em PHP [Laravel], banco de dados MySQL e frontend em HTML, CSS e JavaScript.',
-        //, oferece busca eficiente, sistema de avaliação por provas e certificação PDF automática
-        linkVerProjeto: 'https://plataformatech.com.br',
-        linkRepositorio: 'https://github.com/degrandelucas/Plataforma_AD-Belem'
-    },
-    {
-        titulo: 'Página Grupo de Louvor Renascer',
-        imagem: './img/Pagina-Grupo-De-Louvor.png',
-        altImagem: 'Site_Grupo_De_Louvor',
-        descricao: 'Projeto web para o Grupo Renascer (Igreja Assembléia de Deus Belém) que centraliza letras, vídeos e cifras de louvores (ensaiados, próximo ensaio e futuros). Utilizando JavaScript para gerenciar dinamicamente as listas e renderizá-las em uma interface responsiva construída com HTML, CSS e Materialize CSS, o site facilita o acesso e o estudo dos repertórios do grupo.',
-        linkVerProjeto: 'https://degrandelucas.github.io/Grupo-Louvor-Igreja/',
-        linkRepositorio: 'https://github.com/degrandelucas/Grupo-Louvor-Igreja'
-    },
-    {
-        titulo: 'Sorteador por Planilha Online',
-        imagem: './img/Sorteador-Por-Planilha.png',
-        altImagem: 'Interface-do-Sorteador-por-Planilha',
-        descricao: 'Esta é uma aplicação web desenvolvida para simplificar e tornar eficiente o processo de sorteio através do carregamento de planilhas. Os usuários podem enviar arquivos em formatos como .xlsx, .xls, .csv e .ods, contendo a lista de participantes. A ferramenta processa esses dados para realizar um sorteio de forma aleatória. O backend da aplicação é construído em PHP, utilizando um banco de dados MySQL para armazenar os resultados, enquanto o frontend dinâmico é desenvolvido com HTML, CSS e JavaScript para uma experiência interativa.',
-        linkVerProjeto: 'https://www.linkedin.com/posts/activity-7309656503852896256-OFdf/?utm_source=share&utm_medium=member_desktop&rcm=ACoAABkRIm4BGsrBZm3OHAei3OUigVS8-XtjyJE',
-        linkRepositorio: 'https://github.com/degrandelucas/SorteadorUploadPlanilha'
-    },
-    {
-        titulo: 'Sorteador de Nomes em Lote',
-        imagem: './img/Sorteador_Nomes_Lote.JPG',
-        altImagem: 'Sorteador-de-Nomes-em-Lote',
-        descricao: 'Este projeto é uma aplicação web interativa para sorteio de nomes em lote, desenvolvida utilizando HTML, CSS e JavaScript. Ela permite que o usuário adicione uma lista de nomes separados por vírgula, realize sorteios aleatórios e reinicie o processo facilmente.',
-        linkVerProjeto: 'https://degrandelucas.github.io/SorteadorNomesEmLote/',
-        linkRepositorio: 'https://github.com/degrandelucas/SorteadorNomesEmLote'
-    }
-    // ,
-    // {
-    //     titulo: 'Jogo do Número Secreto',
-    //     imagem: './img/JogoNumeroSecreto.JPG',
-    //     altImagem: 'Jogo-Numero-Secreto',
-    //     descricao: 'O "Jogo do Número Secreto" é uma aplicação web responsiva onde o jogador deve adivinhar um número gerado aleatoriamente entre 1 e 100. Com feedback dinâmico e contagem de tentativas, o jogo proporciona uma experiência divertida e desafiadora. Desenvolvido com HTML, CSS e JavaScript.',
-    //     linkVerProjeto: 'https://degrandelucas.github.io/NumeroSecreto/',
-    //     linkRepositorio: 'https://github.com/degrandelucas/NumeroSecreto'
-    // },
-    // {
-    //     titulo: 'Calculadora IMC',
-    //     imagem: './img/Calculadora-IMC.JPG',
-    //     altImagem: 'Calculadora-IMC',
-    //     descricao: 'A "Calculadora de IMC" é uma aplicação web que permite calcular o Índice de Massa Corporal (IMC) com base no peso e na altura fornecidos pelo usuário. A aplicação é desenvolvida com HTML, CSS e JavaScript, apresentando uma interface intuitiva e orientações sobre a classificação do IMC, como peso ideal, sobrepeso ou obesidade.',
-    //     linkVerProjeto: 'https://degrandelucas.github.io/CalculadoraIMC/',
-    //     linkRepositorio: 'https://github.com/degrandelucas/CalculadoraIMC'
-    // }
+  {
+    titulo: "Plataforma de Cursos Online",
+    imagem: "./img/Plataforma-Estudos-ADBelem.png",
+    altImagem: "Acesso_Login_Plataforma_Estudos_ADBelem",
+    descricao:
+      "Plataforma de E-learning para aprendizado em vídeo, que organiza os cursos divididos em categorias intuitivas. Desenvolvida com backend em PHP [Laravel], banco de dados MySQL e frontend em HTML, CSS e JavaScript.",
+    //, oferece busca eficiente, sistema de avaliação por provas e certificação PDF automática
+    linkVerProjeto: "https://plataformatech.com.br",
+    linkRepositorio: "https://github.com/degrandelucas/Plataforma_AD-Belem",
+  },
+  {
+    titulo: "Página Grupo de Louvor Renascer",
+    imagem: "./img/Pagina-Grupo-De-Louvor.png",
+    altImagem: "Site_Grupo_De_Louvor",
+    descricao:
+      "Projeto web para o Grupo Renascer (Igreja Assembléia de Deus Belém) que centraliza letras, vídeos e cifras de louvores (ensaiados, próximo ensaio e futuros). Utilizando JavaScript para gerenciar dinamicamente as listas e renderizá-las em uma interface responsiva construída com HTML, CSS e Materialize CSS, o site facilita o acesso e o estudo dos repertórios do grupo.",
+    linkVerProjeto: "https://degrandelucas.github.io/Grupo-Louvor-Igreja/",
+    linkRepositorio: "https://github.com/degrandelucas/Grupo-Louvor-Igreja",
+  },
+  {
+    titulo: "Sorteador de Nomes em Lote",
+    imagem: "./img/Sorteador_Nomes_Lote.JPG",
+    altImagem: "Sorteador-de-Nomes-em-Lote",
+    descricao:
+      "Este projeto é uma aplicação web interativa para sorteio de nomes em lote, desenvolvida utilizando HTML, CSS e JavaScript. Ela permite que o usuário adicione uma lista de nomes separados por vírgula, realize sorteios aleatórios e reinicie o processo facilmente.",
+    linkVerProjeto: "https://degrandelucas.github.io/SorteadorNomesEmLote/",
+    linkRepositorio: "https://github.com/degrandelucas/SorteadorNomesEmLote",
+  },
+  {
+    titulo: "Sorteador por Planilha Online",
+    imagem: "./img/Sorteador-Por-Planilha.png",
+    altImagem: "Interface-do-Sorteador-por-Planilha",
+    descricao:
+      "Esta é uma aplicação web desenvolvida para simplificar e tornar eficiente o processo de sorteio através do carregamento de planilhas. Os usuários podem enviar arquivos em formatos como .xlsx, .xls, .csv e .ods, contendo a lista de participantes. A ferramenta processa esses dados para realizar um sorteio de forma aleatória. O backend da aplicação é construído em PHP, utilizando um banco de dados MySQL para armazenar os resultados, enquanto o frontend dinâmico é desenvolvido com HTML, CSS e JavaScript para uma experiência interativa.",
+    linkVerProjeto:
+      "https://www.linkedin.com/posts/activity-7309656503852896256-OFdf/?utm_source=share&utm_medium=member_desktop&rcm=ACoAABkRIm4BGsrBZm3OHAei3OUigVS8-XtjyJE",
+    linkRepositorio: "https://github.com/degrandelucas/SorteadorUploadPlanilha",
+  },
+  // ,
+  // {
+  //     titulo: 'Jogo do Número Secreto',
+  //     imagem: './img/JogoNumeroSecreto.JPG',
+  //     altImagem: 'Jogo-Numero-Secreto',
+  //     descricao: 'O "Jogo do Número Secreto" é uma aplicação web responsiva onde o jogador deve adivinhar um número gerado aleatoriamente entre 1 e 100. Com feedback dinâmico e contagem de tentativas, o jogo proporciona uma experiência divertida e desafiadora. Desenvolvido com HTML, CSS e JavaScript.',
+  //     linkVerProjeto: 'https://degrandelucas.github.io/NumeroSecreto/',
+  //     linkRepositorio: 'https://github.com/degrandelucas/NumeroSecreto'
+  // },
+  // {
+  //     titulo: 'Calculadora IMC',
+  //     imagem: './img/Calculadora-IMC.JPG',
+  //     altImagem: 'Calculadora-IMC',
+  //     descricao: 'A "Calculadora de IMC" é uma aplicação web que permite calcular o Índice de Massa Corporal (IMC) com base no peso e na altura fornecidos pelo usuário. A aplicação é desenvolvida com HTML, CSS e JavaScript, apresentando uma interface intuitiva e orientações sobre a classificação do IMC, como peso ideal, sobrepeso ou obesidade.',
+  //     linkVerProjeto: 'https://degrandelucas.github.io/CalculadoraIMC/',
+  //     linkRepositorio: 'https://github.com/degrandelucas/CalculadoraIMC'
+  // }
 ];
